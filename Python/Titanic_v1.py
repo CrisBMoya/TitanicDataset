3de6726ddd41c894c5e76801a09a56d68d028bb0
CrisBMoya <tobal.92@gmail.com>
2019-10-27 16:06:58 -0300

    update

--- Python/Titanic_v1.py
+++ Python/Titanic_v1.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import pandas_profiling
 import os
 import re
+from sklearn.impute import SimpleImputer
 
 #Set Working Directory
 os.chdir(re.sub(pattern='Python', repl='', string=os.getcwd()))
@@ -37,6 +38,51 @@ SummaryCabin=ClassCabin.groupby(by=['Pclass','Deck']).size()
 SummaryCabin=pd.DataFrame(SummaryCabin).reset_index()
 SummaryCabin.columns=['Pclass','Deck','n']
 
+#Plot
+sns.barplot(x=SummaryCabin['Deck'], y=SummaryCabin['n'], hue=SummaryCabin['Pclass'])
+
 #Compute Probabilities
 SummaryCabin['Prob']=np.concatenate([SummaryCabin[SummaryCabin['Pclass']==X+1]['n']*100/int(SummaryCabin.groupby(by=['Pclass']).sum().iloc[X]) for X in range(0,3)])
 
+#Add deck to data based on distribution
+TrainDF['Deck']='NA'
+for i in range(0, TrainDF.shape[0]):
+    if TrainDF['Cabin'].isna()[i]:
+        DeckChoice=np.random.choice(a=SummaryCabin[SummaryCabin['Pclass']==TrainDF['Pclass'][i]]['Deck'], size=1, p=SummaryCabin[SummaryCabin['Pclass']==TrainDF['Pclass'][i]]['Prob']/100)
+        TrainDF.loc[i, 'Deck']=DeckChoice
+    else:
+        TrainDF.loc[i, 'Deck']=TrainDF.loc[i, 'Cabin']        
+pass
+
+#Add Deck
+TrainDF['Deck']=TrainDF['Deck'].str.replace('\d+', '')
+TrainDF['Deck']=TrainDF['Deck'].astype(str).str[0]
+
+#Imput Ages
+Imp_v1=SimpleImputer(missing_values=np.nan, strategy='mean')
+Imp_v1.fit(X=TrainDF[['Age']])
+Imput_Age=Imp_v1.transform(X=TrainDF[['Age']])
+TrainDF['Age']=Imput_Age
+
+#Delete Cabin column
+TrainDF.drop(columns='Cabin', inplace=True)
+
+#Delete that one entry in embarked
+TrainDF.dropna(inplace=True)
+
+#Delete the one 'T' deck row
+TrainDF.drop(labels=TrainDF[TrainDF['Deck']=='T'].index, inplace=True)
+
+#Missmap
+sns.heatmap(TrainDF.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+
+#Delete non usefull columns
+TrainDF.drop(columns=['PassengerId','Name','Ticket'], inplace=True)
+
+#Create dummy variables
+TrainDF=pd.concat([TrainDF, pd.get_dummies(TrainDF[['Sex','Embarked','Deck']])], axis=1)
+
+#Delete old columns
+TrainDF.drop(columns=['Sex','Embarked','Deck'], inplace=True)
+
+#Split Data
